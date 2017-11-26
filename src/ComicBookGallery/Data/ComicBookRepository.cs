080ded0360a82b812cf08084ecc9b2cdfcc1a70c
0ut1ss <33423069+0ut1ss@users.noreply.github.com>
2017-11-26 12:01:14 +0200

    Added Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -57,6 +57,10 @@ namespace ComicBookGallery.Data
         Favorite = false
     }
     };
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn  = null;
